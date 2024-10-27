fdfd3526dc869b7a25ffba6569b6e515fcbeb520
wiphy <lm802085@gmail.com>
2024-10-27 16:48:23 +0100

    feat: init angular app

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ dotnet dev-certs https --trust
 ```
 
 To find certs `Win + R` and type `certmgr.msc` then `Personnal > Certificates`.
+
+# Find which version of Angular you cant to
+
+Go to [version compatibility](https://angular.dev/reference/versions) to find which `node.js`versions you should use for a specific Angular version.
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe(console.log);
+  }
+}
